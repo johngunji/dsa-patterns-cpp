d789d69878512b549bdd03ea1c01310ecaf435a0
John Gunji <johngunji05@gmail.com>
2025-12-17 14:37:36 +0530

    Add optimized Two Sum using hashing
    Implemented the twoSum function using a hash map to find indices of two numbers that add up to the target value.

--- Arrays/two_sum.cpp
+++ Arrays/two_sum.cpp
@@ -1,1 +1,27 @@
-#include<iostream>
+/*
+Problem:
+Given an array of integers and a target value, find indices of two numbers
+such that they add up to the target.
+Approach:
+Use a hash map to store previously seen values and their indices.
+For each element, check if (target - current) exists in the map.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int need = target-nums[i];
+        if (mp.find(need) != mp.end()) {
+            return {mp[need], i};
+        }
+        mp[nums[i]] = i;
+    }
+    return {};
+}
